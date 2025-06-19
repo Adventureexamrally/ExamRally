c29fc88f1abe0f7caed2ba299026f550d672ec8f
Manikandan-5 <merndev05@gmail.com>
2025-06-19 17:48:31 +0530

    apiupdate

--- src/service/Api.js
+++ src/service/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Api = axios.create({
   baseURL: import.meta.env.VITE_APP_API_BASE_URL,
-  // withCredentials: true,
+  withCredentials: true,
 });
 
 export default Api;
